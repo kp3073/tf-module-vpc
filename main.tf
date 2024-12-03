60b117587fd7b61afd0d5247dec99e53cccf4254
kp3073 <keyurpatel8288@gmail.com>
2024-12-02 22:58:17 -0500

    kp | Robosho terraform code

--- main.tf
+++ main.tf
@@ -1,51 +1,51 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   tags = {
-	Name = "${var.env}-vpc"
+    Name = "${var.env}-vpc"
   }
 }
 
 resource "aws_subnet" "public_subnet" {
-  count = length(var.public_subnet)
+  count             = length(var.public_subnet)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet[count.index]
   availability_zone = var.azs[count.index]
 
   tags = {
-	Name = "public-subnet-${count.index+1}"
+    Name = "public-subnet-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "web_subnet" {
-  count = length(var.web_subnet)
+  count             = length(var.web_subnet)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.web_subnet[count.index]
   availability_zone = var.azs[count.index]
 
   tags = {
-	Name = "web-subnet-${count.index+1}"
+    Name = "web-subnet-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "app_subnet" {
-  count = length(var.app_subnet)
+  count             = length(var.app_subnet)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.app_subnet[count.index]
   availability_zone = var.azs[count.index]
 
   tags = {
-	Name = "app-subnet-${count.index+1}"
+    Name = "app-subnet-${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "db_subnet" {
-  count = length(var.db_subnet)
+  count             = length(var.db_subnet)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.db_subnet[count.index]
   availability_zone = var.azs[count.index]
 
   tags = {
-	Name = "db-subnet-${count.index+1}"
+    Name = "db-subnet-${count.index + 1}"
   }
 }
 
@@ -53,14 +53,14 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-	Name = "${var.env}-igw"
+    Name = "${var.env}-igw"
   }
 }
 
 resource "aws_eip" "eip" {
   domain = "vpc"
   tags = {
-	name = "${var.env}-ngw"
+    name = "${var.env}-ngw"
   }
 }
 
@@ -70,7 +70,7 @@ resource "aws_nat_gateway" "ngw" {
   subnet_id     = aws_subnet.public_subnet[0].id
 
   tags = {
-	Name = "${var.env}-ngw"
+    Name = "${var.env}-ngw"
   }
 }
 
@@ -85,27 +85,27 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-	cidr_block = "0.0.0.0/0"
-	gateway_id = aws_internet_gateway.igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-	Name = "public"
+    Name = "public"
   }
 }
 resource "aws_route_table" "web" {
   vpc_id = aws_vpc.main.id
 
   route {
-	cidr_block     = "0.0.0.0/0"
-	nat_gateway_id = aws_nat_gateway.ngw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   route {
-	cidr_block                = var.default_vpc_cidr
-	vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
   tags = {
-	Name = "web"
+    Name = "web"
   }
 }
 
@@ -113,16 +113,16 @@ resource "aws_route_table" "app" {
   vpc_id = aws_vpc.main.id
 
   route {
-	cidr_block     = "0.0.0.0/0"
-	nat_gateway_id = aws_nat_gateway.ngw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   route {
-	cidr_block                = var.default_vpc_cidr
-	vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
   tags = {
-	Name = "app"
+    Name = "app"
   }
 }
 
@@ -130,16 +130,16 @@ resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
 
   route {
-	cidr_block     = "0.0.0.0/0"
-	nat_gateway_id = aws_nat_gateway.ngw.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
   route {
-	cidr_block                = var.default_vpc_cidr
-	vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+    cidr_block                = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
   }
   tags = {
-	Name = "db"
+    Name = "db"
   }
 }
 
@@ -150,25 +150,25 @@ resource "aws_route" "default-route-tabel" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet)
+  count          = length(var.public_subnet)
   subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "web" {
-  count = length(var.web_subnet)
+  count          = length(var.web_subnet)
   subnet_id      = aws_subnet.web_subnet[count.index].id
   route_table_id = aws_route_table.web.id
 }
 
 resource "aws_route_table_association" "app" {
-  count = length(var.app_subnet)
+  count          = length(var.app_subnet)
   subnet_id      = aws_subnet.app_subnet[count.index].id
   route_table_id = aws_route_table.app.id
 }
 
 resource "aws_route_table_association" "db" {
-  count = length(var.db_subnet)
+  count          = length(var.db_subnet)
   subnet_id      = aws_subnet.db_subnet[count.index].id
   route_table_id = aws_route_table.db.id
 }
